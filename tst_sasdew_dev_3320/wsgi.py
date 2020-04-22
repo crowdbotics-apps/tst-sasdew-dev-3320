e2b179ac32659316d8e54407b17509f942dafcd9
Crowdbotics <team@crowdbotics.com>
2020-04-22 14:22:31 +0000

    Update models

--- tst_sasdew_dev_3320/wsgi.py
+++ tst_sasdew_dev_3320/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_sasdew_dev_3320.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tst_sasdew_dev_3320.settings")
 
 application = get_wsgi_application()
